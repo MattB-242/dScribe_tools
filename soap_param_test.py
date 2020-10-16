b697f217b58c9fb0ab894519ef48505f1678ddd7
MattB-242 <58733062+MattB-242@users.noreply.github.com>
2020-10-16 12:31:49 +0100

    Update soap_param_test.py

--- soap_param_test.py
+++ soap_param_test.py
@@ -10,7 +10,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from dscribe.descriptors import SOAP
-from dscribe.kernels import AverageKernel
 from ase import Atoms
 from ase.io import read
 
